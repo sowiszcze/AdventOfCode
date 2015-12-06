420ccccbd33a23169804babdcc959ac40a9f7b55
Piotr Sowa <sowa@secred.pl>
2015-12-06 16:53:24 +0100

    Added solution for part 2 of day 5

---
+++ AdventOfCode/Solution10.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Interaction logic for Solution1.xaml
+    /// </summary>
+    [AOCSolution("Day 5, part 2")]
+    public partial class Solution10 : SolutionBase
+    {
+        private const int MINIMUM_VOWELS = 3;
+        private const int MINIMUM_DUPLICATES = 2;
+
+        public Solution10()
+        {
+            InitializeComponent();
+            this.Title = base.GetTitle();
+        }
+
+        private void Process(object sender, RoutedEventArgs e)
+        {
+            ToggleUi(false);
+
+            this.ResultTextBox.Text = this.InputTextBox
+                                          .Text
+                                          .Split('\n', '\r')
+                                          .Select(s => s.Trim())
+                                          .Where(s => !string.IsNullOrWhiteSpace(s) &&
+                                                      Check(s))
+                                          .Count()
+                                          .ToString();
+
+            ToggleUi(true);
+        }
+
+        private void ToggleUi(bool toggle)
+        {
+            this.ProcessButton.IsEnabled = toggle;
+            this.ResultTextBox.IsEnabled = toggle;
+            this.InputTextBox.IsEnabled = toggle;
+        }
+
+        private bool Check(string check)
+        {
+            bool predicate1 = false;
+            bool predicate2 = false;
+            for (int i = 0; i < check.Length - 1 && !predicate1; ++i)
+            {
+                string first = check.Substring(i, 2);
+                for (int j = i + 2; j < check.Length - 1 && !predicate1; ++j)
+                {
+                    if (first == check.Substring(j, 2))
+                    {
+                        predicate1 = true;
+                    }
+                }
+            }
+
+            for (int i = 0; i < check.Length - 2 && !predicate2; ++i)
+            {
+                if (check[i] == check[i + 2])
+                {
+                    predicate2 = true;
+                }
+            }
+
+            return predicate1 && predicate2;
+        }
+    }
+}
